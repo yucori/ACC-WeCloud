ed382d1676671eb8ae1cfa3e55e86185788beab1
SeoYoung <seoyoung7623@naver.com>
2024-07-28 01:42:53 +0900

    init: 초기세팅

---
+++ src/main/java/wecloud/wishpool/WishPoolApplication.java
@@ -0,0 +1,13 @@
+package wecloud.wishpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishPoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wecloud/wishpool/WishPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package wecloud.wishpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
