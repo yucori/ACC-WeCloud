9ecf7b47eecaa89d21684428b40faecd88ad451c
SeoYoung <seoyoung7623@naver.com>
2024-07-28 12:31:19 +0900

    feat: openAPI swagger 적용

---
+++ src/main/java/wecloud/wishpool/global/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package wecloud.wishpool.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("WishPool API 명세서")
+                .version("v0.0.1")
+                .description("WishPool API 명세서입니다.");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
