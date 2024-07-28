830c18f397c91d538d4a5ccfe0233dd91fb5e432
SeoYoung <seoyoung7623@naver.com>
2024-07-28 13:54:24 +0900

    feat: security 수정

--- src/main/java/wecloud/wishpool/WishPoolApplication.java
+++ src/main/java/wecloud/wishpool/WishPoolApplication.java
@@ -2,12 +2,12 @@ package wecloud.wishpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WishPoolApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WishPoolApplication.class, args);
     }
