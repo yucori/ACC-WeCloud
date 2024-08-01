f1ff8df0f9c71661bd21e3046828862fbd5ff864
SeoYoung <seoyoung7623@naver.com>
2024-08-01 23:15:46 +0900

    feat: 후기 S3 이미지 등록

--- src/main/java/wecloud/wishpool/domain/review/controller/ReviewController.java
+++ src/main/java/wecloud/wishpool/domain/review/controller/ReviewController.java
@@ -3,11 +3,16 @@ package wecloud.wishpool.domain.review.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import wecloud.wishpool.domain.review.dto.request.ReviewCreateRequestDto;
 import wecloud.wishpool.domain.review.dto.response.ReviewGetResponseDto;
 import wecloud.wishpool.domain.review.service.ReviewService;
+import wecloud.wishpool.global.aws.service.S3Service;
 import wecloud.wishpool.global.response.ApiResponse;
+
+import java.io.IOException;
 
 @Tag(name = "Review API", description = "리뷰 API")
 @RestController
@@ -15,10 +20,14 @@ import wecloud.wishpool.global.response.ApiResponse;
 @RequestMapping("/wishes/{wishId}/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
-    @PostMapping
+    private final S3Service s3Service;
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "리뷰 생성", description = "리뷰를 생성합니다.")
-    public ApiResponse<Long> createReview(@PathVariable Long wishId, @RequestBody ReviewCreateRequestDto requestDto) {
-        Long reviewId = reviewService.createReview(wishId, requestDto);
+    public ApiResponse<Long> createReview(@PathVariable Long wishId,
+                                          @RequestPart ReviewCreateRequestDto requestDto,
+                                          @RequestPart(value = "img", required = false) MultipartFile multipartFile) throws IOException {
+        String reviewImg = s3Service.upload(multipartFile, "review");
+        Long reviewId = reviewService.createReview(wishId,reviewImg, requestDto);
         return ApiResponse.response201Success(reviewId, "리뷰 생성 완료");
     }
 
@@ -28,5 +37,4 @@ public class ReviewController {
         ReviewGetResponseDto reviewDto = reviewService.findReview(reviewId, wishId);
         return ApiResponse.responseSuccess(reviewDto, "리뷰 조회 완료");
     }
-
 }
